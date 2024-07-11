d5a63ead923917ccf63cbce8283e5a46e74cb9e3
Mick-Thomson <mechanical_dildo@mail.ru>
2024-07-11 16:37:58 +0300

    add

--- src/main/java/com/thomson/questproject/servlet/QuestionServlet.java
+++ src/main/java/com/thomson/questproject/servlet/QuestionServlet.java
@@ -19,7 +19,7 @@ import java.util.List;
 import static com.thomson.questproject.consts.WebConsts.*;
 
 /**
- * Класс-сервлет для question.jsp и страницы с путём /question
+ * Класс-сервлет для /question
  */
 @WebServlet(name = "questionServlet", value = "/question")
 public class QuestionServlet extends HttpServlet implements DefaultServlet {
